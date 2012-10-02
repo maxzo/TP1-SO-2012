28ed66044cd8c1f4343cee479b78bad4d4fe521e
maxzo <sariego.isi@gmail.com>
2012-10-02 20:01:54 -0300

    Update eje4.sh

--- eje4.sh
+++ eje4.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 if [ $# -eq 2 ] ; then
-	cd $2
-	if [ $1 = 'l' ] ; then
-		ls -l --format=single-column
-	elif [ $1 = 't' ] ; then
-		ls -F --classify > temp.txt
-		sed -i 's/$/ - fichero regular/g' temp.txt
-		sed -i 's/* - fichero regular$/ - fichero ejecutable/g' temp.txt
-		sed -i 's/\/ - fichero regular$/ - directorio/g' temp.txt
-		sed -i 's/@ - fichero regular$/ - enlace simbólico/g' temp.txt
-		sed -i 's/| - fichero regular$/ - FIFO/g' temp.txt
-		sed -i 's/= - fichero regular$/ - directorio/g' temp.txt
-		cat temp.txt
-		rm temp.txt
-	elif [ $1 = 'i' ] ; then
-		echo "Ha ingresado \"i\""
+	cd /
+	if [[ -d $2 ]] ; then
+		cd $2
+	else
+		echo "Error: Directorio inválido"
+		exit
 	fi
+fi
+
+if [ $1 = 'l' ] ; then
+	ls -l --format=single-column
+elif [ $1 = 't' ] ; then
+	ls -F --classify > temp.txt
+	sed -i '/temp.txt/d' temp.txt
+	sed -i 's/$/ -- fichero regular/g' temp.txt
+	sed -i 's/* -- fichero regular$/ -- fichero ejecutable/g' temp.txt
+	sed -i 's/\/ -- fichero regular$/ -- directorio/g' temp.txt
+	sed -i 's/@ -- fichero regular$/ -- enlace simbólico/g' temp.txt
+	sed -i 's/| -- fichero regular$/ -- FIFO/g' temp.txt
+	sed -i 's/= -- fichero regular$/ -- directorio/g' temp.txt
+	cat temp.txt
+	rm temp.txt
+elif [ $1 = 'i' ] ; then
+	ls -i
 fi
