33343fc768b438fecfbe419d4c3782ed7bcfafff
maxzo <sariego.isi@gmail.com>
2012-10-05 11:30:47 -0300

    Actualización eje2.sh

---
+++ eje2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Bienvenido, usuario: $(whoami)"
+echo "Usted se ha conectado el día: $(date +"%d/%m/%Y")"
+echo "En la terminal: $(tty)"
+echo "Se encuentran conectados en este momento, los siguientes usuarios:"
+who | awk '{ print $1 }'
