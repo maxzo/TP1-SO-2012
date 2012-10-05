1ab62ee4c4dff6531ef4d774247907606c555beb
maxzo <sariego.isi@gmail.com>
2012-10-05 12:58:25 -0300

    Actualización eje6.sh

---
+++ eje6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $3 -gt 0 ] ; then
+	sort $1 > $2
+	echo "" >> $2
+	echo "Cantidad de líneas: $(wc -l < $2)" >> $2
+	echo "Cantidad de palabras: $(wc -w < $2)" >> $2
+	echo "Cantidad de caracteres: $(wc -m < $2)" >> $2
+	
+	for ((i = 1; i < $3 ; i++)) ; do
+		echo "" >> $2
+		sort $1 >> $2
+		echo "" >> $2
+		echo "Cantidad de líneas: $(wc -l < $2)" >> $2
+		echo "Cantidad de palabras: $(wc -w < $2)" >> $2
+		echo "Cantidad de caracteres: $(wc -m < $2)" >> $2
+	done
+fi
