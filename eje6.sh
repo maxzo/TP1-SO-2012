a70136d689ff6601e70864261ef40a359fe9d8d4
gmichaut <gmichaut@gmail.com>
2012-10-05 22:56:03 -0500

    script 100%

--- eje6.sh
+++ eje6.sh
@@ -6,7 +6,7 @@ if [ $3 -gt 0 ] ; then
 	echo "Cantidad de líneas: $(wc -l < $2)" >> $2
 	echo "Cantidad de palabras: $(wc -w < $2)" >> $2
 	echo "Cantidad de caracteres: $(wc -m < $2)" >> $2
-	
+
 	for ((i = 1; i < $3 ; i++)) ; do
 		echo "" >> $2
 		sort $1 >> $2
