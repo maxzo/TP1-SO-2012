de99794e388714e0ac1d7d1d47f85933c8baa155
Gabriel Michaut <gmichaut@gmail.com>
2012-10-11 20:29:48 -0300

    ejercicio 7 OK

--- eje7.sh
+++ eje7.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+cd $1
+la | sort
+echo ""
+if [ $(ls -d c* | wc -w -ne 0 ] ; then
+	echo "Existen archivos que comienzan con la letra c"
+else
+	echo "NO existen archivos que comiencen con la letra c"
+fi
