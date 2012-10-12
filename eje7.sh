8f1398b582750793e69283e65f735d034db62684
maxzo <sariego.isi@gmail.com>
2012-10-11 21:23:14 -0300

    Actualización eje7.sh

--- eje7.sh
+++ eje7.sh
@@ -3,8 +3,8 @@
 cd $1
 ls | sort
 echo ""
-if [ $(ls -d c* | wc -w -ne 0 ] ; then
+if [ $(ls -d c* 2> /dev/null | wc -w) -ne 0 ] ; then
 	echo "Existen archivos que comienzan con la letra c"
 else
-	echo "NO existen archivos que comiencen con la letra c"
+	echo "No existen archivos que comiencen con la letra c"
 fi
