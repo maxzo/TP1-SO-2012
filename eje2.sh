f6e110045365e38756de29762c5fd8b293da51ed
maxzo <sariego.isi@gmail.com>
2012-10-12 10:47:57 -0300

    Update eje2.sh
    Faltaba mostrar la hora

--- eje2.sh
+++ eje2.sh
@@ -2,6 +2,7 @@
 
 echo "Bienvenido, usuario: $(whoami)"
 echo "Usted se ha conectado el día: $(date +"%d/%m/%Y")"
+echo "A la hora: $(date +"%T")"
 echo "En la terminal: $(tty)"
 echo "Se encuentran conectados en este momento, los siguientes usuarios:"
 who | awk '{ print $1 }'
